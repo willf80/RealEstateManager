8ca760379b3a1b0243c8d5fe83466c1de4729b0a
willf80 <willyfkouadio@gmail.com>
2019-06-03 23:44:26 +0200

    Bug #1 fixed activity_second_activity_text_view_main resources id not existed and can't set integer to string. Quantity are reformatted with string format

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMain;
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
@@ -29,6 +31,6 @@ public class MainActivity extends AppCompatActivity {
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         this.textViewQuantity.setTextSize(20);
-        this.textViewQuantity.setText(quantity);
+        this.textViewQuantity.setText(String.format(Locale.getDefault(), "%d", quantity));
     }
 }
