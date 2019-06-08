c53bbc9979c1d1e34a015645fa5da466c950f9e4
willf80 <willyfkouadio@gmail.com>
2019-06-08 17:41:26 +0200

    Property medias view added with his adapter

--- app/src/main/java/com/openclassrooms/realestatemanager/fragments/DetailsPropertyFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/fragments/DetailsPropertyFragment.java
@@ -5,21 +5,34 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.adapters.MediaAdapter;
+import com.openclassrooms.realestatemanager.models.Media;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetailsPropertyFragment extends Fragment {
     private static final String ARG_PROPERTY_ID = "propertyId";
 
+    RecyclerView mMediaRecyclerView;
+
     private String mPropertyId;
 
     private OnDetailsDispatchListener mListener;
+    private MediaAdapter mMediaAdapter;
+    private List<Media> mMediaList;
 
     public static DetailsPropertyFragment newInstance(String propertyId) {
         DetailsPropertyFragment fragment = new DetailsPropertyFragment();
@@ -42,7 +55,16 @@ public class DetailsPropertyFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_details_property, container, false);
-        ButterKnife.bind(view);
+        mMediaRecyclerView = view.findViewById(R.id.mediaRecyclerView);
+
+        mMediaList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            mMediaList.add(new Media());
+        }
+
+        mMediaAdapter = new MediaAdapter(mMediaList);
+        mMediaRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        mMediaRecyclerView.setAdapter(mMediaAdapter);
 
         return view;
     }
