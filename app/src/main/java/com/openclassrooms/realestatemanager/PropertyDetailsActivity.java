2fa2638e86d6c2b14b8be533bf6f06a083268688
willf80 <willyfkouadio@gmail.com>
2019-06-08 15:18:51 +0200

    Second activity renamed to PropertyDetailsActivity to show details in phone mode

--- app/src/main/java/com/openclassrooms/realestatemanager/PropertyDetailsActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/PropertyDetailsActivity.java
@@ -3,11 +3,11 @@ package com.openclassrooms.realestatemanager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SecondActivity extends AppCompatActivity {
+public class PropertyDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_property_details);
     }
 }
